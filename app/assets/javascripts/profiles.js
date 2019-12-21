6c28bc809b54295b9a4d2578f205c73264e7cf49
Carlos Rodríguez <carlosdr9812@gmail.com>
2019-12-20 19:30:22 -0500

    Rating stars in all te sites, and input rating stars

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -433,4 +433,48 @@ jQuery(document).ready(function($) {
   }
 
  }
+
+ $(function () {	
+	$(".rateYoCustom").each(function(){
+		if ( $(this).attr("rating") != null || $(this).attr("rating") != undefined || $(this).attr("rating") != "" ) {
+			if ($(this).attr("starsSize") == "normal") {
+				$(this).rateYo({
+					rating: $(this).attr("rating"),
+					ratedFill: "#f1c40f",
+					normalFill: "#e4e4e4",
+					readOnly: true,
+					starWidth: "30px"
+				});
+			}
+			if ($(this).attr("starsSize") == "small") {
+				$(this).rateYo({
+					rating: $(this).attr("rating"),
+					ratedFill: "#f1c40f",
+					normalFill: "#e4e4e4",
+					readOnly: true,
+					starWidth: "20px"
+				});	
+			}
+		}			
+ 	});
+
+ 	var $rateYo = $(".rateYoInput").rateYo({
+ 		rating: 3,
+ 		ratedFill: "#f1c40f",
+		normalFill: "#e4e4e4",
+		onSet: function (){
+			var rating = $rateYo.rateYo("rating");
+			window.alert("Its " + rating + " Yo!");
+			console.log(rating);
+			var input = document.getElementById("rateExperience");
+		 	if (input) {
+		 		input.value = rating;
+		 		console.log(input.value, rating);
+		 	}
+		}
+ 	}); 	 	
+ });
+
 })
+
+
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -4,5 +4,38 @@ jQuery(document).ready(function($) {
         let selected = event.target;
     });
     
+	$(function () {	
+		$(".rateYoCustom").each(function(){
+		if ( $(this).attr("rating") != null || $(this).attr("rating") != undefined || $(this).attr("rating") != "" ) {
+			if ($(this).attr("starsSize") == "normal") {
+				$(this).rateYo({
+					rating: $(this).attr("rating"),
+					ratedFill: "#f1c40f",
+					normalFill: "#e4e4e4",
+					readOnly: true,
+					starWidth: "30px"
+				});
+			}
+			if ($(this).attr("starsSize") == "small") {
+				$(this).rateYo({
+					rating: $(this).attr("rating"),
+					ratedFill: "#f1c40f",
+					normalFill: "#e4e4e4",
+					readOnly: true,
+					starWidth: "20px"
+				});	
+			}
+		}			
+		});
 
+		var $rateYo = $(".rateYoInput").rateYo({
+			rating: 3,
+			ratedFill: "#f1c40f",
+		normalFill: "#e4e4e4",
+		onSet: function (){
+			var rating = $rateYo.rateYo("rating");
+			window.alert("Its " + rating + " Yo!");
+		}
+		}); 	
+	});
 })
